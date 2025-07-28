32605c0299f7edffa19274b5fbad597e44fe47dd
diegodev <diegodev.manriquez@gmail.com>
2025-07-28 18:19:56 -0400

    Add logging for TradingView iframe's contentWindow upon detection

--- index.js
+++ index.js
@@ -14,6 +14,7 @@
   
       if (target) {
         console.log('✅ ****Iframe encontrado:', target);
+        console.log('✅ ****Iframe encontrado:', target.contentWindow);
         clearInterval(intervalId);
       } else {
         waited += INTERVAL;
